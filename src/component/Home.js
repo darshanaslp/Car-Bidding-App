0f99317692ff3394a5f719dd70962ff7f2b30844
darshanaslp <darshanaslp@hotmail.com>
2023-08-21 10:15:22 +0530

    feat:add spinner

--- src/component/Home.js
+++ src/component/Home.js
@@ -17,6 +17,7 @@ const Home = () => {
     const error = useSelector(state => state.error);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(true);
 
     // Pagination state
     const [currentPage, setCurrentPage] = useState(1);
@@ -24,11 +25,33 @@ const Home = () => {
 
     // Add state for selected brand
     const [selectedBrand, setSelectedBrand] = useState('All');
-    const brands = ['All', ' Volkswagen', 'Audi', 'Ford', 'Mercedes', 'BMW'];
+    const brands = ['All', 'Volkswagen', 'Audi', 'Ford', 'Mercedes', 'BMW'];
 
     useEffect(() => {
-        dispatch(fetchVehiclesAction());
+      const fetchData = async () => {
+        try {
+          await dispatch(fetchVehiclesAction());
+          setIsLoading(false)
+        } catch (error) {
+          setIsLoading(false);
+          toast.error(`Data fetch error: ${error.message}`);
+        }
+      };
+      fetchData();
     }, [dispatch]);
+
+
+    if (isLoading) {
+      return (
+        <div className="container mt-4">
+          <div className="text-center">
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        </div>
+      );
+    }
 
 
     // Handle brand filter change
@@ -62,6 +85,12 @@ const Home = () => {
     };
 
 
+    //default image
+    const defaultBrandImages = {
+        Volkswagen: 'https://www.carlogos.org/logo/Volkswagen-logo-2000-1920x1080.png',
+      };
+
+
     if (error) {
         return <p>Error: {error}</p>;
     }
@@ -76,88 +105,97 @@ const Home = () => {
 
     return (
         <div className="container mt-4">
-            <div className="row">
-                <div className="col-md-2">
-                    <h4>Filter by Brand</h4>
-                    <select
-                        value={selectedBrand}
-                        onChange={(e) => handleBrandFilterChange(e.target.value)}
-                        className="form-select"
-                    >
-                        {brands.map((brand) => (
-                            <option key={brand} value={brand}>
-                                {brand}
-                            </option>
+            <div className="container mt-4">
+                <div className="row">
+                    <div className="col-md-3">
+                        <h4>Filter by Brand</h4>
+                        <select
+                            value={selectedBrand}
+                            onChange={(e) => handleBrandFilterChange(e.target.value)}
+                            className="form-select"
+                        >
+                            {brands.map((brand) => (
+                                <option key={brand} value={brand}>
+                                    {brand}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+                {filteredVehicles && filteredVehicles.length === 0 ? (
+                    <div className="alert alert-warning mt-3" role="alert">
+                        No cars available for the selected brand.
+                    </div>
+                ) : (
+                    <div className="row pt-3">
+                        {(currentVehicles).map((vehicle) => (
+                            <div className="col-sm-3" key={vehicle.id}>
+                                <div className="card border-info">
+                                    <img className="card-img-top" src={vehicle.details.image || defaultBrandImages.Volkswagen } alt={vehicle.details.brand}/>
+                                    <div className="card-body">
+                                        <Link to={`/vehicle/${vehicle.id}`}>
+                                            <h5 className="card-title">{vehicle.name}</h5>
+                                        </Link>
+                                        <p className="card-text">{vehicle.details.description}</p>
+                                        <p className="card-text">Price: {vehicle.details.currency} {vehicle.details.price}</p>
+                                    </div>
+                                    <div className="card-footer">
+
+
+                                        {/* Bidding Form */}
+                                        <Formik
+                                            initialValues={{ bidAmount: '', vehicleId: vehicle.id }} // Initialize vehicleId
+                                            validationSchema={Yup.object({
+                                                bidAmount: Yup.number()
+                                                    .typeError('Bid amount must be a number')
+                                                    .integer('Bid amount must be an integer')
+                                                    .min(vehicle.details.price + 1, `Bid must be higher than ${vehicle.details.currency} ${vehicle.details.price + 1}`)
+                                                    .required('Required'),
+                                            })}
+                                            onSubmit={handleSubmitBid}
+                                        >
+                                            {({ isValid, touched }) => (
+                                                <Form>
+                                                    <div className="form-group">
+                                                        <Field
+                                                            type="number"
+                                                            name="bidAmount"
+                                                            placeholder="Enter bid amount"
+                                                            className="form-control"
+                                                        />
+                                                        <ErrorMessage name="bidAmount" component="div" className="text-danger" />
+                                                    </div>
+                                                    <button type="submit" className="btn btn-primary" disabled={!isValid || !touched.bidAmount}>
+                                                        Submit Bid
+                                                    </button>
+                                                </Form>
+                                            )}
+                                        </Formik>
+                                    </div>
+                                </div>
+                            </div>
                         ))}
-                    </select>
+                    </div>
+                )}
+                <div className='pt-2'>
+                    <ReactPaginate
+                        previousLabel={'Previous'}
+                        nextLabel={'Next'}
+                        breakLabel={'...'}
+                        containerClassName={'pagination'}
+                        activeClassName={'active'}
+                        previousLinkClassName={'page-link'}
+                        nextLinkClassName={'page-link'}
+                        activeLinkClassName={'active'}
+                        pageClassName={'page-link'}
+                        pageCount={Math.ceil((filteredVehicles || vehicles).length / vehiclesPerPage)}
+                        onPageChange={handlePageChange}
+
+                    />
                 </div>
             </div>
-            <div className="row pt-3">
-                {(currentVehicles).map((vehicle) => (
-                    <div className="col-sm-3" key={vehicle.id}>
-                        <div className="card border-info">
-                            <img className="card-img-top" src={vehicle.details.image} alt={vehicle.details.brand} />
-                            <div className="card-body">
-                                <Link to={`/vehicle/${vehicle.id}`}>
-                                    <h5 className="card-title">{vehicle.name}</h5>
-                                </Link>
-                                <p className="card-text">{vehicle.details.description}</p>
-                                <p className="card-text">Price: {vehicle.details.currency} {vehicle.details.price}</p>
-                            </div>
-                            <div className="card-footer">
-
-
-                                {/* Bidding Form */}
-                                <Formik
-                                    initialValues={{ bidAmount: '', vehicleId: vehicle.id }} // Initialize vehicleId
-                                    validationSchema={Yup.object({
-                                        bidAmount: Yup.number()
-                                            .typeError('Bid amount must be a number')
-                                            .integer('Bid amount must be an integer')
-                                            .min(vehicle.details.price + 1, `Bid must be higher than ${vehicle.details.currency} ${vehicle.details.price + 1}`)
-                                            .required('Required'),
-                                    })}
-                                    onSubmit={handleSubmitBid}
-                                >
-                                    {({ isValid, touched }) => (
-                                        <Form>
-                                            <div className="form-group">
-                                                <Field
-                                                    type="number"
-                                                    name="bidAmount"
-                                                    placeholder="Enter bid amount"
-                                                    className="form-control"
-                                                />
-                                                <ErrorMessage name="bidAmount" component="div" className="text-danger" />
-                                            </div>
-                                            <button type="submit" className="btn btn-primary" disabled={!isValid || !touched.bidAmount}>
-                                                Submit Bid
-                                            </button>
-                                        </Form>
-                                    )}
-                                </Formik>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <div className='pt-2'>
-            <ReactPaginate
-                previousLabel={'Previous'}
-                nextLabel={'Next'}
-                breakLabel={'...'}
-                containerClassName={'pagination'}
-                activeClassName={'active'}
-                previousLinkClassName={'page-link'}
-                nextLinkClassName={'page-link'}
-                activeLinkClassName={'active'}
-                pageClassName={'page-link'}
-                pageCount={Math.ceil((filteredVehicles || vehicles).length / vehiclesPerPage)}
-                onPageChange={handlePageChange}
-
-            />
-            </div>
         </div>
+
     );
 };
 
